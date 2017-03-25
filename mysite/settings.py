5e2d293a536f40779c6b6a5b3961b42d90fd8877
Carla <carlafiadi@gmail.com>
2017-03-25 15:36:01 -0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g@%bak&mp6+0qvzq+4rlz=0##!9j#m&%&yb!vdclxenlbksuzm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["carlafiadi.pythonanywhere.com"]
+ALLOWED_HOSTS = ["carlafiadi.pythonanywhere.com", '127.0.0.1']
 
 
 # Application definition
